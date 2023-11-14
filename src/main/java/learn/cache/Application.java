39fd725810db71010ce62a3fef60043473e88073
Krevis <venzersiz@gmail.com>
2023-11-15 07:21:58 +0900

    Clean codes

--- src/main/java/learn/cache/Application.java
+++ src/main/java/learn/cache/Application.java
@@ -3,14 +3,12 @@ package learn.cache;
 import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-@Slf4j
 public class Application {
 
     private final ApplicationContext applicationContext;
