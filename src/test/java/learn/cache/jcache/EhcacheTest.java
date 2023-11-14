4f70f734137db11c691ac0ee334010d072e06026
Krevis <venzersiz@gmail.com>
2023-11-14 20:51:30 +0900

    Cache provider > JCache (JSR-107) > Ehcache 3 > Cache expiry

--- src/test/java/learn/cache/jcache/EhcacheTest.java
+++ src/test/java/learn/cache/jcache/EhcacheTest.java
@@ -20,20 +20,40 @@ class EhcacheTest {
     void getCache() {
         assertThat(cacheManager).isInstanceOf(JCacheCacheManager.class); // Eh107CacheManager
 
-        Cache cacheA = cacheManager.getCache("a");
+        Cache cacheA = cacheManager.getCache("a"); // application.yml
         assertThat(cacheA).isInstanceOf(JCacheCache.class);
 
-        Cache cacheB = cacheManager.getCache("b");
-        assertThat(cacheB).isNull();
+        Cache cacheB = cacheManager.getCache("b"); // ehcache.xml
+        assertThat(cacheB).isInstanceOf(JCacheCache.class);
+
+        Cache cacheC = cacheManager.getCache("c"); // ehcache.xml
+        assertThat(cacheC).isInstanceOf(JCacheCache.class);
     }
 
     @Test
     void putAndGet() {
-        Cache cacheA = cacheManager.getCache("a");
+        Cache cacheA = cacheManager.getCache("a"); // application.yml
 
         cacheA.put("key1", "value1");
         assertThat(cacheA.get("key1").get()).isEqualTo("value1");
-
         assertThat(cacheA.get("key2")).isNull();
+
+        Cache cacheB = cacheManager.getCache("b"); // ehcache.xml
+
+        cacheB.put("key1", "value1");
+        assertThat(cacheB.get("key1").get()).isEqualTo("value1");
+        assertThat(cacheB.get("key2")).isNull();
+    }
+
+    @Test
+    void expiry() throws InterruptedException {
+        Cache cache = cacheManager.getCache("usingExpiry"); // TTL: 5초
+
+        cache.put("key1", "value1");
+        assertThat(cache.get("key1").get()).isEqualTo("value1");
+
+        Thread.sleep(5_000);
+
+        assertThat(cache.get("key1")).isNull(); // 캐쉬 만료됨
     }
 }
