400e168a2e5df78305e88508d69321b2a39eb7aa
Krevis <venzersiz@gmail.com>
2023-11-14 20:57:08 +0900

    Cache provider > JCache (JSR-107) > Ehcache 3 > Value as object

--- src/test/java/learn/cache/jcache/EhcacheTest.java
+++ src/test/java/learn/cache/jcache/EhcacheTest.java
@@ -2,6 +2,10 @@ package learn.cache.jcache;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.Serializable;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,5 +59,38 @@ class EhcacheTest {
         Thread.sleep(5_000);
 
         assertThat(cache.get("key1")).isNull(); // 캐쉬 만료됨
+    }
+
+    @Test
+    void valueAsObject() {
+        Cache cache = cacheManager.getCache("a");
+
+        List<Address> addresses = List.of(new Address("주소1"), new Address("주소2"));
+        User user1 = new User(1L, "김백세", addresses);
+        cache.put("user1", user1);
+
+        User cachedUser1 = (User) cache.get("user1").get();
+        assertThat(cachedUser1.getSeq()).isEqualTo(1L);
+        assertThat(cachedUser1.getName()).isEqualTo("김백세");
+        assertThat(cachedUser1.getAddresses().get(0).getName()).isEqualTo("주소1");
+        assertThat(cachedUser1.getAddresses().get(1).getName()).isEqualTo("주소2");
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    static class Address implements Serializable {
+
+        private final String name;
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    static class User implements Serializable {
+
+        private final Long seq;
+
+        private final String name;
+
+        private final List<Address> addresses;
     }
 }
