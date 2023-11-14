a8a0853a65961601f07f311f19ddb3488fdd0b31
Krevis <venzersiz@gmail.com>
2023-11-14 13:29:51 +0900

    디버깅을 위해 스프링 실행 시 모든 빈 이름 출력

--- src/main/java/learn/cache/Application.java
+++ src/main/java/learn/cache/Application.java
@@ -1,12 +1,26 @@
 package learn.cache;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 public class Application {
+
+    private final ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
